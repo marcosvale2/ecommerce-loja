8233d8e969597f7ed51ac1320f2a4ff08cf59d7a
marcosvale2 <marcosviniciussoaresdovale@gmail.com>
2025-11-13 22:14:12 -0300

    Delete fix erro 3

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,11 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    origin: "*", // Pode deixar liberado por enquanto
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
   })
 );
-
 
 app.use(express.json());
 
